035138815ae86a6d74dc17a344809c23766d4a40
unknown <MH@MH-PC.(none)>
2012-06-07 14:00:06 +0300

    kayttoliittyma aloitettu

--- TiraMain/src/kayttoliittyma/KL.java
+++ TiraMain/src/kayttoliittyma/KL.java
@@ -4,10 +4,46 @@
  */
 package kayttoliittyma;
 
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import logiikka.LabyrinttiSolver;
+
 /**
  *
  * Ohjelman käyttöliittymä. Tästä tullee kevyt graafinen liittymä.
  */
-public class KL {
-    
+public class KL implements Runnable {
+
+    private LabyrinttiSolver solveri;
+    private JFrame frame;
+
+    /**
+     * Käyttöliitymä saa parametrina LabyrinttiSolver olion, jonka kautta se
+     * pääsee käsiksi sovelluslogiikan luokkiin.
+     *
+     * @param solveri
+     */
+    public KL(LabyrinttiSolver solveri) {
+        this.solveri = solveri;
+    }
+
+    public void run() {
+        frame = new JFrame("Magee Labyrintti");
+        frame.setPreferredSize(new Dimension(200, 100));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        luoKomponentit(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void luoKomponentit(Container contentPane) {
+        JPanel paneeli = new JPanel();
+        
+    }
 }
--- TiraMain/src/tiramain/Main.java
+++ TiraMain/src/tiramain/Main.java
@@ -5,6 +5,9 @@
 
 package tiramain;
 
+import javax.swing.SwingUtilities;
+import kayttoliittyma.KL;
+import logiikka.Bittikartta;
 import logiikka.Koordinaatti;
 import logiikka.Labyrintti;
 import logiikka.LabyrinttiSolver;
@@ -19,8 +22,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Labyrintti laby = new Labyrintti();
+        Bittikartta kartta = new Bittikartta("ekaLaby.png");
+        Labyrintti laby = new Labyrintti(kartta.getLabyrintti());
         LabyrinttiSolver solveri = new LabyrinttiSolver(laby);
+        KL kayttoliittyma = new KL(solveri);
+        SwingUtilities.invokeLater(kayttoliittyma);
+//        Labyrintti laby = new Labyrintti();
+//        LabyrinttiSolver solveri = new LabyrinttiSolver(laby);
 //        Koordinaatti[][] naatit = solveri.dijkstra();
         
 //        for (Koordinaatti[] koordinaattis : naatit) {
